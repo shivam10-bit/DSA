064a45550924eee0f10a920bbbbc9437fafcd96f
Shivam shrivastava <shivamshrivatava004@gmail.com>
2025-08-10 02:46:19 +0530

    Update maximumSubarray.cpp

--- maximumSubarray.cpp
+++ maximumSubarray.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Initialize 'maxi' with the first element,
-        // or a very small number if the array can be empty.
-        // Using INT_MIN is safer if negative numbers are possible.
+        
         long long sum = 0;
-        long long maxi = nums[0]; // Initialize with the first element
+        long long maxi = nums[0]; 
 
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
@@ -14,8 +12,7 @@ public:
                 maxi = sum;
             }
             
-            // If the current sum becomes negative, it's better
-            // to start a new subarray from the next element.
+      
             if (sum < 0) {
                 sum = 0;
             }
