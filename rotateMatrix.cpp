c8babef91d0f2abb3602b1d3b484d3ff11548962
Shivam <shivamshrivatava004@gmail.com>
2025-08-16 23:48:45 +0530

    inital commit

---
+++ rotateMatrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    Solution sol;
+    sol.rotate(matrix);
+
+    cout << "Rotated matrix:" << endl;
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
