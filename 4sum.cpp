63195b858b5464b74dc837f13570c94a9268e244
Shivam <shivamshrivatava004@gmail.com>
2025-08-22 23:40:29 +0530

    initial commit

---
+++ 4sum.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        if (n < 4) return result;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+                
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicates
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        
+                        // skip duplicates for left/right
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        
+                        left++;
+                        right--;
+                    }
+                    else if (sum < target) left++;
+                    else right--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 0, -1, 0, -2, 2}; // Example input
+    int target = 0; // Example target
+    vector<vector<int>> result = sol.fourSum(nums, target);
+
+    cout << "4Sum results:" << endl;
+    for (const auto& quad : result) {
+        for (int num : quad) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
